595be29ecc4d6a6c6b008a72da3f5fc91c6d3890
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-05-09 00:12:22 +0530

    Remove ERC1155 reward functions from automation

--- contracts/Stack3Automation.sol
+++ contracts/Stack3Automation.sol
@@ -6,26 +6,46 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Stack3.sol";
 import "./Stack3RareMintNFT.sol";
 
 contract Stack3Automation is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
 
+    uint256 public constant USER = 0;
+    uint256 public constant QUESTION_10 = 1;
+    uint256 public constant QUESTION_25 = 2;
+    uint256 public constant QUESTION_50 = 3;
+    uint256 public constant QUESTION_100 = 4;
+    uint256 public constant ANSWER_10 = 5;
+    uint256 public constant ANSWER_25 = 6;
+    uint256 public constant ANSWER_50 = 7;
+    uint256 public constant ANSWER_100 = 8;
+    uint256 public constant ANSWER_200 = 9;
+    uint256 public constant ANSWER_500 = 10;
+    uint256 public constant BEST_ANSWER_10 = 11;
+    uint256 public constant BEST_ANSWER_50 = 12;
+    uint256 public constant BEST_ANSWER_250 = 13;
+    uint256 public constant COMMENTS_100 = 14;
+    uint256 public constant COMMENTS_500 = 15;
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 5;
+
 
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
     uint32 private immutable i_callbackGasLimit;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
-    uint32 private constant NUM_WORDS = 5;
-
+    
     uint256 private immutable i_rareMintDropInterval;
     Stack3RareMintNFT private immutable i_rareNft;
     Stack3 private immutable i_stack3;
     uint256 private s_lastUpkeepTimestamp;
     uint256 private s_randomRewardsMaxSupply;
     mapping (address => bool) s_rareMintReceived;
+    address [] s_topUsers;
 
     uint256 private s_randomRewardsCounter;
     
@@ -66,6 +86,11 @@ contract Stack3Automation is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownab
     }
 
 
+    function setTopUsers (address [] memory _topUsers) external onlyOwner {
+        s_topUsers = _topUsers;
+    }
+
+
     function performUpkeep(
         bytes calldata /* performData */
     ) external override {
@@ -93,10 +118,10 @@ contract Stack3Automation is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownab
         uint256[] memory randomWords
     ) internal override {
 
-        address [] memory topUsers = i_stack3.getTopUsers();
+        // address [] memory topUsers = s_topUsers;
         
         for (uint256 i = 0; i < randomWords.length; i++) {
-            address winner = topUsers[randomWords[i] % topUsers.length];
+            address winner = s_topUsers[randomWords[i] % s_topUsers.length];
 
             if(!s_rareMintReceived[winner] && i_rareNft.balanceOf(winner) <= 3) {
                 address collection = i_rareNft.collectionMintAddress();
@@ -106,6 +131,8 @@ contract Stack3Automation is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownab
             }
         }
     }
+
+    
 
 
     
