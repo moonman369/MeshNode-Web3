347439470694410fc27b37ab76bad8200d868980
Vijay Kumar <vijaykumardevktg@gmail.com>
2023-05-16 21:56:32 +0630

    set hardhat with my config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
 
   networks: {
-    sepolia: {
-      url: "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      accounts: [process.env.PRIVATE_KEY_S],
-      chainId: 11155111,
-      saveDeployments: true,
-    },
+    // sepolia: {
+    //   url: 'https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    //   accounts: [process.env.PRIVATE_KEY_S],
+    //   chainId: 11155111,
+    //   saveDeployments: true,
+    // },
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/",
+      url: 'https://rpc-mumbai.maticvigil.com/',
       accounts: [process.env.PRIVATE_KEY_S],
       chainId: 80001,
       saveDeployments: true,
